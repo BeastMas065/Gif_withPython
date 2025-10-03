6e7fa5a3257538e816e13dda5abc5e95baae0799
BeastMas <masgamer065@gmail.com>
2025-10-03 15:32:53 +0530

    Add files via upload

---
+++ GIF_withpython.py
@@ -0,0 +1,9 @@
+import imageio.v3 as iio
+
+filenames = ['team_pic1.png', 'team_pic2.png']
+images = [ ]
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('teamgif.gif', images, duration = 500, loop = 0)
